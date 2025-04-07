cd0e16d1993472f1a9f0719d9a739dd6f6e9cdfe
esepp22 <stefhen33@gmail.com>
2025-04-08 04:25:51 +0700

    Update BackButton.tsx

--- app/dashboard/testimoni/BackButton.tsx
+++ app/dashboard/testimoni/BackButton.tsx
@@ -10,7 +10,7 @@ export function BackButton() {
   return (
     <button
       onClick={handleBack}
-      className="absolute cursor-pointer h-[47px] left-[71px] top-[33px] w-[38px] max-sm:top-5 max-sm:left-5 max-sm:h-[37px] max-sm:w-[30px]"
+      className="absolute cursor-pointer h-[47px] left-[71px] top-[33px] w-[38px] max-sm:top-6 max-sm:left-5 max-sm:h-[37px] max-sm:w-[30px]"
     >
       <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/a3de98da703a87cfa6b86626e48de17289530cee" alt="Back" />
     </button>
