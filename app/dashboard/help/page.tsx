10454bbdba0b92eb0d259e3ba7100614dfb93011
stepe-ee <steve.anggana@gmail.com>
2025-04-23 03:45:15 +0700

    bg

--- app/dashboard/help/page.tsx
+++ app/dashboard/help/page.tsx
@@ -43,7 +43,7 @@ export default function HelpPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-gradient-to-b from-white to-teal-100 py-12 px-4 sm:px-6 lg:px-8">
       <Head>
         <title>Customer Service - Help Center</title>
       </Head>
