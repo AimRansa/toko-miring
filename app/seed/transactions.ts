29224ca50ed98b25d5fc2122fbb4254b8ba5b8be
esepp22 <stefhen33@gmail.com>
2025-04-22 22:43:01 +0700

    eseepp gcor

---
+++ app/seed/transactions.ts
@@ -0,0 +1,18 @@
+// seed/transactions.ts
+export const transactions = [
+    {
+      id: 1,
+      customerName: 'Budi Santoso',
+      carName: 'Ferrari 488',
+      date: '2024-04-01',
+      amount: 3500000000,
+    },
+    {
+      id: 2,
+      customerName: 'Rina Marlina',
+      carName: 'Lamborghini Aventador',
+      date: '2024-04-02',
+      amount: 4500000000,
+    },
+  ];
+  
