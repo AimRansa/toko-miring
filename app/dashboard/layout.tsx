ff85e5fff1661e0b0a6180d9f78a7ef21a3c814f
AimRansa <ransaaim@gmail.com>
2025-06-11 14:39:33 +0700

    ilangin sedikit isu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,16 @@
 import "@/ui/global.css";
 import { inter } from "@/ui/fonts";
 
+export const metadata = {
+  title: "Toko Miring",
+  description: "Aplikasi Manajemen Toko",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={inter.className}>
       <body>
-        <main>{children}</main>
+        {children}
       </body>
     </html>
   );
